e65666a7cc62f631d55a273035ca5ed878986fb3
jakobns <98582450+jakobns@users.noreply.github.com>
2023-01-20 08:40:59 +0100

    oppdateringer

--- ARBEIDSKRAV/ressurserAK.js
+++ ARBEIDSKRAV/ressurserAK.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openCity(evt, category) {
       // Declare all variables
       var i, tabcontent, tablinks;
     
@@ -15,6 +15,6 @@ function openCity(evt, cityName) {
       }
     
       // Show the current tab, and add an "active" class to the button that opened the tab
-      document.getElementById(cityName).style.display = "block";
+      document.getElementById(category).style.display = "block";
       evt.currentTarget.className += " active";
     } 
